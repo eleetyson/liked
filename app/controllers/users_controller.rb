39c80fcfd7ff362443c544db1f9cb79cdb5afa5e
Ethan <eleetyson@gmail.com>
2021-01-09 17:11:33 -0500

    add to users#show and build out follow model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,17 @@ class UsersController < ApplicationController
   def home
   end
 
-  # passes a client object from application#client as an argument to a user instance method
-  # the user instance method fetches user's Twitter follows, randomly selects one, and returns the account info as an object
+  # fetches authenticating user's Twitter follows, randomly selects one, and instantiates a Follow object with that account info
+  # fetches recent likes from account represented by the Follow object, randomly picks one, and instantiates a Like object with that tweet info
+  #
   def show
     redirect_to user_path(current_user) if !authorized?(params[:id])
-    binding.pry
-    # follow = current_user.get_random_follow(client)
-    # Follow.create(uid: follow.attrs[:id_str], name: follow.attrs[:name], handle: follow.attrs[:screen_name], user_id: current_user.id)
+
+    follow = current_user.get_random_follow(client)
+    @follow = Follow.create(uid: follow.attrs[:id_str], name: follow.attrs[:name], handle: follow.attrs[:screen_name], user_id: current_user.id)
+
+    like = @follow.get_random_like(client)
+    @like = Like.create(tid: like.attrs[:id_str], follow_id: @follow.id)
   end
 
 end
--- app/models/follow.rb
+++ app/models/follow.rb
@@ -4,4 +4,16 @@ class Follow < ApplicationRecord
   validates :name, presence: true
   validates :handle, presence: true
 
+# returns an account's uid attribute as an integer
+  def get_uid_int
+    self.uid.to_i
+  end
+
+# fetches and cleans an array of an account's recently favorited Tweets
+# returns a randomly selected Tweet from that array, as an object
+  def get_random_like(client)
+    likes = client.favorites(self.get_uid_int)
+    like = likes.sample
+  end
+
 end
