8432e49cc0e438122c0755f00e4f33e13f9e106c
Ethan <eleetyson@gmail.com>
2021-01-09 09:49:29 -0500

    move logic from sessions controller into user model and add ar validations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,19 @@
 class SessionsController < ApplicationController
 
 # GET /auth/twitter/callback
-# logs user in given valid credentials
-# redirects to sign on page if not
+# logs user on
+# finds existing account or creates a new one
+# stores user id in session hash for reference
   def create
-    @user = User.find_or_create_by(uid: auth['uid']) do |u|
-      u.name = auth[:info][:name]
-      u.handle = auth[:info][:nickname]
-      u.token = auth[:credentials][:token]
-      u.secret = auth[:credentials][:secret]
-    end
-
-    session[:user_id] = @user.id
-    redirect_to user_path(@user)
+    # user = User.find_or_create_by(uid: auth['uid']) do |u|
+    #   u.name = auth[:info][:name]
+    #   u.handle = auth[:info][:nickname]
+    #   u.token = auth[:credentials][:token]
+    #   u.secret = auth[:credentials][:secret]
+    # end
+    user = User.from_omniauth(auth)
+    session[:user_id] = user.id
+    redirect_to user_path(user)
   end
 
 # POST /logout
@@ -24,7 +25,7 @@ class SessionsController < ApplicationController
 
   private
 
-# helper method to make parsing omniauth response simpler
+  # helper method to make parsing omniauth response simpler
   def auth
     request.env['omniauth.auth']
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ApplicationRecord
   has_many :follows
+  validates :name, presence: true
+  validates :handle, presence: true, uniqueness: true
+  validates :token, presence: true, uniqueness: true
+  validates :secret, presence: true, uniqueness: true
+
+# finds existing user or creates new user based on Twitter API response
+  def self.from_omniauth(auth)
+    self.find_or_create_by(uid: auth['uid']) do |u|
+      u.name = auth[:info][:name]
+      u.handle = auth[:info][:nickname]
+      u.token = auth[:credentials][:token]
+      u.secret = auth[:credentials][:secret]
+    end
+  end
+
 end
