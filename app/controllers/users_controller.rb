936e2bf8531ef35f9cc8363b16ee2ca673bb4342
Ethan <eleetyson@gmail.com>
2021-01-10 11:05:18 -0500

    stub out basic show view with embedded tweet

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   # fetches authenticating user's Twitter follows, randomly selects one, and instantiates a Follow object with that account info
   # fetches recent likes from account represented by the Follow object and randomly picks one
-  # uses that tweet's information to generate logic for embedding it and also creates a corresponding Like object
+  # uses that tweet's information to create a corresponding Like object and generate logic for embedding it in the view
   def show
     redirect_to user_path(current_user) if !authorized?(params[:id])
 
@@ -15,9 +15,8 @@ class UsersController < ApplicationController
     @follow = Follow.create(uid: follow.attrs[:id_str], name: follow.attrs[:name], handle: follow.attrs[:screen_name], user_id: current_user.id)
 
     like = @follow.get_random_like(client)
-    # @like = Like.create(tid: like.attrs[:id_str], follow_id: @follow.id)
-
-    # @embedded_tweet = client.oembed(@like.get_tid_int)
+    @like = Like.create(tid: like.attrs[:id_str], follow_id: @follow.id)
+    @embedded_tweet = client.oembed(@like.get_tid_int)
   end
 
 end
